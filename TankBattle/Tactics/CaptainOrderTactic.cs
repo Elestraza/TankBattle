41e5758f3496742c38831ff91ca67cac714b2dfe
Elestraza <gameswithiy@gmail.com>
2026-06-30 11:50:23 +0300

    Captain Order Tactic: цель выбранная капитаном Type Prioriy Tacric: цель выбирается на основе типа танка атакующего и танка врага

---
+++ TankBattle/Tactics/CaptainOrderTactic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TankBattle.Classes;
+
+namespace TankBattle.Tactics
+{
+    class CaptainOrderTactic : Tactic
+    {
+        public override Tank SelectTarget(Tank attacker, List<Tank> enemies)
+        {
+            Random index = new(enemies.Count);
+
+            Tank[] aliveEnemies = enemies.Where(t => t.IsAlive && t.IsTarget == false).ToArray();
+            Tank selectedTank = aliveEnemies[index.Next(0, aliveEnemies.Length)];
+            selectedTank.IsTarget = true;
+            return selectedTank;
+        }
+    }
+}
---
+++ TankBattle/Tactics/TypePriorityTactic.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TankBattle.Classes;
+using TankBattle.Tanks;
+
+namespace TankBattle.Tactics
+{
+    class TypePriorityTactic : Tactic
+    {
+        /*
+         По приоритету типов - каждый тип атакует определенного противника:
+	        Тяжелый -> средний
+	        Средний -> легкий
+	        Легкий -> случайная цель
+         */
+        public override Tank SelectTarget(Tank attacker, List<Tank> enemies)
+        {
+
+            if (attacker is HeavyTank)
+                return enemies.Where(t => t is MediumTank).First();
+            if (attacker is MediumTank)
+                return enemies.Where(t => t is LightTank).First();
+            
+            // if attacker is LightTank
+            Random index = new(enemies.Count);
+
+            Tank[] aliveEnemies = enemies.Where(t => t.IsAlive && t.IsTarget == false).ToArray();
+            Tank selectedTank = aliveEnemies[index.Next(0, aliveEnemies.Length)];
+            selectedTank.IsTarget = true;
+            return selectedTank;
+        }
+    }
+}
